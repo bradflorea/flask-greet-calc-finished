4a2e90606879e9cf4c377dbac0e0f8d9b192e6e3
bradflorea <bradleyaflorea@gmail.com>
2024-07-08 18:26:12 -0400

    first function done

--- calc/app.py
+++ calc/app.py
@@ -1,4 +1,15 @@
-from flask import Flask
-
+from flask import Flask, request
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
+
+
+@app.route("/add")
+def add_nums():
+    "Add a and b numbers."
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
