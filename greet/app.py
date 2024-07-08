fd719771d631869ca8b2b140015dbf197524db55
bradflorea <bradleyaflorea@gmail.com>
2024-07-08 17:58:59 -0400

    finished

--- greet/app.py
+++ greet/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/welcome")
 def welcome():
     return "Welcome!"
+
+
+@app.route("/welcome/back")
+def welcome_back():
+    return "welcome back"
+
+
+@app.route("/welcome/home")
+def welcome_home():
+    return "welcome home"
